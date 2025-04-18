35218402afcaa1c34f3afd1710b61f8af95c0749
dubidubob <2076397@ewhain.net>
2025-04-18 15:42:52 +0900

    [Test] 임시
    [Fixing] 앞 raycast를 자기 자신이 인식
    
    Physics2D.queriesStartInColliders 안 먹힘
    
    [Fix] 앞 raycast를 자기 자신이 인식하는 이슈 고침
    
    TODO : class FSM으로 관리

--- Zombie_Circular/Assets/2. Scripts/TestZombieMove.cs
+++ Zombie_Circular/Assets/2. Scripts/TestZombieMove.cs
@@ -4,52 +4,80 @@ using UnityEngine;
 
 public class TestZombieMove : MonoBehaviour
 {
-    [SerializeField] private float jumpForce = 1f;  // 점프 시 힘
-    [SerializeField] private float pushRightForce = 1f;  // 점프 시 오른쪽 힘
-    [SerializeField] private float moveSpeed = 1f;  // 달릴 시 힘
-    private Rigidbody2D rb;
-    private bool canRun = true;
-
-    private bool isBeingPushed = false;
-    private float pushThreshold = 1.0f;
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    enum ZombieState
+    { 
+        Run,
+        Stop,
+        IsPushed,
+        Jump
     }
 
-    private void Update()
+    [SerializeField] private float jumpForce = 1f;  // 점프 시 힘
+    [SerializeField] private float moveSpeed = 1f;  // 달릴 시 힘
+    public float rayDistance = 1f;
+    public LayerMask groundLayer;
+    public LayerMask zombieLayer;
+    private Rigidbody2D rb;
+    private ZombieState state;
+    private RaycastHit2D downHit;
+    
+
+    private void Awake()
     {
-        // 달리기
-        if (canRun && !isBeingPushed)
-        {
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-        }
+        Physics2D.queriesStartInColliders = false;
+        rb = GetComponent<Rigidbody2D>();
+        state = ZombieState.Run;
     }
 
     private void FixedUpdate()
     {
-        float externalForceX = rb.velocity.x - moveSpeed;
+        var col = GetComponent<Collider2D>();
+        Vector2 origin = (Vector2)col.bounds.center
+                       + Vector2.left * (col.bounds.extents.x);
 
-        if (externalForceX > pushThreshold)
+        RaycastHit2D frontHit = Physics2D.Raycast(
+            origin, Vector2.left, rayDistance,
+            groundLayer | zombieLayer);
+        Debug.DrawLine(origin, origin + Vector2.left * rayDistance, Color.green, 0.1f);
+
+        if (frontHit.collider != null)
         {
-            // 오른쪽으로 밀리는 힘이 감지됨
-            isBeingPushed = true;
-            canRun = false;
+            // 앞에 장애물이 있으면 정지
+            Debug.Log(frontHit.collider.tag);
+            state = ZombieState.Stop;
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
         else
         {
-            isBeingPushed = false;
-            canRun = true;
+            if (state == ZombieState.IsPushed)
+                return;
+            // 앞에 아무것도 없으면 전진
+            Debug.Log("넌 이제 자유다");
+            state = ZombieState.Run;
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y); // 왼쪽으로 이동
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Tower"))
         {
-            canRun = false;
+            downHit = Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer | zombieLayer);
+            Debug.DrawLine(transform.position, transform.position + Vector3.down * rayDistance, Color.red, 60f);
+            if (downHit.collider != null && downHit.collider.CompareTag("Ground"))
+            {
+                if(state != ZombieState.Stop)
+                    state = ZombieState.Stop;
+            }
+            else if (downHit.collider != null && downHit.collider.CompareTag("Monster"))
+            {
+                Debug.Log($"{this.name} 밑에 몬스터~!");
+                if (state!= ZombieState.Run)
+                {
+                    state = ZombieState.Run;
+                }
+            }
         }
-
         else if (collision.gameObject.CompareTag("Monster"))
         {
             // 왼쪽(앞쪽)일 시 점프
@@ -58,33 +86,13 @@ public class TestZombieMove : MonoBehaviour
 
             if (Vector2.Dot(normal, Vector2.right) > 0.9f)
             {
-                Vector2 impulse = Vector2.up * jumpForce
-                        + Vector2.right * pushRightForce;
+                if (state == ZombieState.Jump)
+                    return;
+                state = ZombieState.Jump;
+                Debug.Log($"{this.name} jump!");
+                Vector2 impulse = Vector2.up * jumpForce;
                 rb.AddForce(impulse, ForceMode2D.Impulse);
             }
-            canRun = true;
         }
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        DisableRun();
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        Invoke(nameof(EnableRun), 1f);
-    }
-
-    private void EnableRun()
-    {
-        isBeingPushed = false;
-        canRun = true;
-    }
-
-    private void DisableRun()
-    {
-        isBeingPushed = true;
-        canRun = false;
     }
 }
